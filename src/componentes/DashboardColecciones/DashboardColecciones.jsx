cf8581cb269baa3c4cabd254b63f0c61bee8c749
Cristinarte <cristinarh.nrh@gmail.com>
2025-04-14 18:36:28 +0200

    Modifico importacion en DashboardColecciones

--- src/componentes/DashboardColecciones/DashboardColecciones.jsx
+++ src/componentes/DashboardColecciones/DashboardColecciones.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { Colecciones } from "../Colecciones/Colecciones";
-import "./dashboardColecciones.scss";
+import "./DashboardColecciones.scss";
 
 export const DashboardColecciones = () => {
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
